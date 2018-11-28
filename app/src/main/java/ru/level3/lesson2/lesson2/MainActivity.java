833970c831cdfc5a7d6c6b7f97275e905ff63412
Alexandr Rashchupkin <a.rasschupkin@gmail.com>
2018-11-29 01:52:30 +0300

    commit

--- app/src/main/java/ru/level3/lesson2/lesson2/MainActivity.java
+++ app/src/main/java/ru/level3/lesson2/lesson2/MainActivity.java
@@ -2,8 +2,12 @@ package ru.level3.lesson2.lesson2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Создание переменных
+    EditText txtEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
